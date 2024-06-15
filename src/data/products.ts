6efdac56d6e24ac99e167074ee284f4068c1f7fb
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-15 04:59:33 -0500

    feat: Improve project configuration and database management
    This commit includes the following changes:
    - Added Prisma configuration file for efficient usage of the Prisma client.
    - Refactored global CSS files to adhere to established file structure.
    - Added migration lock file, seed script, and schema definition for improved database management and data seeding capabilities.
    - Updated project configuration files for better code organization, linting, and formatting.
    
    These changes enhance the project's configuration and database management capabilities.

---
+++ src/data/products.ts
@@ -0,0 +1,216 @@
+export const data = {
+  categories: ['Auriculares', 'Celulares', 'Computadoras', 'Consolas'],
+  products: [
+    {
+      type: 'auriculares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 1,
+      stock: 30,
+      slug: 'audifonos-deportivos',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776444/tech%20store/earphones_a_1.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_a_2.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_a_3.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_a_4.webp'
+      ],
+      name: 'Audífonos deportivos',
+      price: 60
+    },
+    {
+      type: 'auriculares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 2,
+      stock: 20,
+      slug: 'audifonos-inalambricos',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_b_1.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_b_2.webp'
+      ],
+      name: 'Audifonos inalambricos',
+      price: 80
+    },
+    {
+      type: 'auriculares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 3,
+      stock: 25,
+      slug: 'audifonos-sports',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_c_1.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/earphones_c_2.webp'
+      ],
+      name: 'Audifonos Sports',
+      price: 120
+    },
+    {
+      type: 'auriculares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 4,
+      stock: 10,
+      slug: 'audifonos-gamer',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-a-1.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-a-2.webp'
+      ],
+      name: 'Audifonos Gamer',
+      price: 250
+    },
+    {
+      type: 'auriculares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 5,
+      stock: 20,
+      slug: 'audifonos-bass',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676832882/tech%20store/headphones_b_1.webp',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676832882/tech%20store/headphones_b_2.webp'
+      ],
+      name: 'Audifonos Bass',
+      price: 200
+    },
+    {
+      type: 'auriculares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 6,
+      stock: 25,
+      slug: 'audifonos-bass',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-c-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/headphones-c-2.png'
+      ],
+      name: 'Audifonos Bass',
+      price: 200
+    },
+    {
+      type: 'celulares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 7,
+      stock: 25,
+      slug: 'iphone-13-pro',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/iphone-13-pro-1.png'
+      ],
+      name: 'Iphone 13 Pro',
+      price: 800
+    },
+    {
+      type: 'celulares',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 8,
+      stock: 5,
+      slug: 'iphone-14-pro',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776545/tech%20store/iphone-14-pro-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776546/tech%20store/iphone-14-pro-2.png'
+      ],
+      name: 'Iphone 14 Pro',
+      price: 1000
+    },
+    {
+      type: 'computadoras',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 9,
+      stock: 5,
+      slug: 'mac-studio-m1',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776543/tech%20store/mac-studio-m1-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/mac-studio-m1-2.png'
+      ],
+      name: 'Mac Studio M1',
+      price: 1200
+    },
+    {
+      type: 'computadoras',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 10,
+      stock: 10,
+      slug: 'macbook-air-m2',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776546/tech%20store/macbook-air-m2-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776546/tech%20store/macbook-air-m2-2.png'
+      ],
+      name: 'MacBook Air M2',
+      price: 900
+    },
+    {
+      type: 'consolas',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 11,
+      stock: 14,
+      slug: 'oculus-quest-2',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/oculus-quest-2-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/oculus-quest-2-2.png'
+      ],
+      name: 'Oculus Quest 2',
+      price: 600
+    },
+    {
+      type: 'consolas',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 12,
+      stock: 20,
+      slug: 'playstation-4-slim',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-4-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-4-2.png'
+      ],
+      name: 'PlayStation 4 Slim',
+      price: 300
+    },
+    {
+      type: 'consolas',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 13,
+      stock: 10,
+      slug: 'playstation-5',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-5-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-5-2.png'
+      ],
+      name: 'PlayStation 5',
+      price: 550
+    },
+    {
+      type: 'consolas',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 14,
+      stock: 10,
+      slug: 'playstation-vr',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-vr-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/playstation-vr-2.png'
+      ],
+      name: 'PlayStation VR',
+      price: 500
+    },
+    {
+      type: 'consolas',
+      description:
+        'Ut faucibus et felis eu hendrerit. Nam pretium risus non laoreet egestas. Integer id risus sed nunc dignissim pretium. Proin in tempus lorem. Fusce pulvinar vehicula felis, eu sagittis leo imperdiet a. Cras convallis suscipit sodales. Curabitur quis porta velit. Sed dui erat, convallis vitae interdum quis, interdum vitae turpis. Etiam et turpis nunc. Suspendisse potenti.',
+      id: 15,
+      stock: 50,
+      slug: 'smartwatch-sport',
+      images: [
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/watch-1.png',
+        'https://res.cloudinary.com/dcmnvayfw/image/upload/v1676776544/tech%20store/watch-2.png'
+      ],
+      name: 'SmartWatch Sport',
+      price: 70
+    }
+  ]
+}
