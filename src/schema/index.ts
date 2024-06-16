b80388c3964635fcd6f68b54d023dac375bfadbc
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-16 03:21:36 -0500

    refactor: Update schema and types to include user and address

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,8 @@
 export * from './address/address-schema'
 
-export * from './cart/cart-schema'
-
 export * from './products/products-schema'
+
+export * from './cart/cart-schema'
 
 export * from './user/user-schema'
 export * from './user/user-login-schema'
