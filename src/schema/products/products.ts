5c517a102a844112dbc78257bdaf33220ba7afb0
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-15 21:37:00 -0500

    feat: Add product types for image and category

---
+++ src/schema/products/products.ts
@@ -0,0 +1,49 @@
+import { z } from 'zod'
+
+const category = z.object({
+  id: z.number(),
+  name: z.string()
+})
+
+const productImage = z.object({
+  id: z.number(),
+  url: z.string(),
+  productId: z.number()
+})
+
+export const ProductsFullDbSchema = z.object({
+  category,
+  productImage: z.array(productImage),
+  id: z.number(),
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  stock: z.number(),
+  slug: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date()
+})
+
+export const ProductsWithCategoryDbSchema = z.object({
+  category,
+  id: z.number(),
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  stock: z.number(),
+  slug: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date()
+})
+
+export const ProductsDbSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  stock: z.number(),
+  slug: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  categoryId: z.number()
+})
