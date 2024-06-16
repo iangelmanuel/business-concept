b7919c6bce7c152a57cc00cd6929ffc88a0b0bf0
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-15 22:23:08 -0500

    refactor: Update schema and types to include user and address
    This commit updates the schema and types in the `src/schema/index.ts` and `src/types/index.d.ts` files to include the `user` and `address` modules. It exports the necessary functions and types for working with user and address data. This change ensures that the user and address functionality is properly integrated into the codebase, improving the overall functionality and maintainability of the application.

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,7 @@
+export * from './address/address'
+
 export * from './cart/cart'
 
 export * from './products/products'
 
-export * from './address/address'
+export * from './user/user'
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,9 @@
+export * from './address/address'
+
 export * from './cart/card'
 
 export * from './products/products'
 export * from './products/products-images'
 export * from './products/products-category'
+
+export * from './user/user'
