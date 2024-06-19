86a61183280b87d45301b4d57d87a053b470e903
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-18 19:00:53 -0500

    refactor: Remove userAddress.deleteMany() call in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,6 +5,7 @@ async function main() {
   await prisma.orderAddress.deleteMany()
   await prisma.orderItem.deleteMany()
   await prisma.order.deleteMany()
+  await prisma.userAddress.deleteMany()
   await prisma.user.deleteMany()
   await prisma.productImage.deleteMany()
   await prisma.product.deleteMany()
