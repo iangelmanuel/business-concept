a6da97d6e0e6a18ee8b7039fb231aa95aeb9f973
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-19 23:18:38 -0500

    refactor: Add AddressForm and LocationType types
    This commit adds the AddressForm and LocationType types to the address.d.ts file. The AddressForm type is a subset of the AddressType, containing specific fields for a form. The LocationType is inferred from the locationSchema. This change improves the codebase by providing more precise types for address-related data.

--- src/types/address/address.d.ts
+++ src/types/address/address.d.ts
@@ -1,4 +1,20 @@
-import type { addressSchema } from '@/schema'
+import type { addressSchema, locationSchema } from '@/schema'
 import type { z } from 'zod'
 
 export type AddressType = z.infer<typeof addressSchema>
+export type AddressForm = Pick<
+  AddressType,
+  | 'firstName'
+  | 'lastName'
+  | 'typeOfIdentification'
+  | 'identification'
+  | 'address'
+  | 'address2'
+  | 'postalCode'
+  | 'department'
+  | 'city'
+  | 'phone'
+  | 'extraData'
+>
+
+export type LocationType = z.infer<typeof locationSchema>
