6165ce7e0eb2c5320eb469cbdc642f024bb20525
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-20 04:59:00 -0500

    feat: Add checkout page

--- app/(shop)/shop/checkout/page.tsx
+++ app/(shop)/shop/checkout/page.tsx
@@ -1,12 +1,14 @@
 import { auth } from '@/auth.config'
+import { CardCheckoutDetails, CardCheckoutSummary } from '@/components'
 import { redirect } from 'next/navigation'
 
 export default async function CheckoutPage() {
   const user = await auth()
   if (!user) redirect('/auth/login?redirect=/shop/checkout')
   return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
+    <section className="grid grid-cols-1 sm:grid-cols-3 gap-x-5 max-w-screen-2xl mx-auto mt-10">
+      <CardCheckoutDetails />
+      <CardCheckoutSummary />
+    </section>
   )
 }
