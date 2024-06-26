bb45498e24b4f91c81413f489fd8548a8aa858d2
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-26 05:31:04 -0500

    refactor: Update ShopProductsPage component
    This commit refactors the `ShopProductsPage` component in the `/app/(shop)/shop/products/page.tsx` file. The changes include removing unused imports, simplifying the card structure, updating the page heading, and improving the layout of the product grid. This update enhances the performance, readability, and user experience of the shop products page.

--- app/(shop)/shop/products/page.tsx
+++ app/(shop)/shop/products/page.tsx
@@ -1,12 +1,5 @@
-import { getAllProducts, getCategories } from '@/actions'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  ProductsButtonCart
-} from '@/components'
+import { getAllProducts } from '@/actions'
+import { Card, CardContent, CardDescription, CardHeader } from '@/components'
 import { fontSans } from '@/config'
 import { formatCurrency } from '@/utils'
 import Image from 'next/image'
@@ -15,68 +8,70 @@ import { notFound } from 'next/navigation'
 
 export default async function ShopProductsPage() {
   const products = await getAllProducts()
-  const categories = await getCategories()
-  if (!products || !categories) return notFound()
+  const lastProduct = products[products.length - 1]
+  if (!products) return notFound()
   return (
     <section className="p-5 2xl:p-0 max-w-screen-2xl mx-auto mt-10">
-      <h1 className={`text-2xl font-bold ${fontSans.className} mb-3`}>
-        Bienvenidos a nuestra tienda online
-      </h1>
-
       <article className="mb-10">
-        <h2 className={`text-xl font-bold ${fontSans.className} mb-2`}>
-          Categorias
-        </h2>
-
-        <section className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-3">
-          {categories.map((category) => (
-            <Link
-              key={category.id}
-              href={`/shop/products/category/${category.name}`}
-              className={`extrabold ${fontSans.className} text-4xl`}
+        <Card className="p-10 flex flex-col lg:flex-row gap-y-5 lg:gap-y-0 justify-center items-center">
+          <section className="max-w-xl">
+            <h1
+              className={`text-2xl md:text-3xl lg:text-5xl font-bold mb-2 ${fontSans.className}`}
             >
-              <Card className="flex items-center justify-center hover:border-gray-400 py-5 capitalize">
-                {category.name}
-              </Card>
-            </Link>
-          ))}
-        </section>
+              Ahora la mágia de comprar en un sitio web rapido y seguro
+            </h1>
+            <CardDescription>
+              En Business Concept manejamos un amplio catálogo de productos para
+              que puedas encontrar lo que necesitas. Desde productos de
+              limpieza, hasta productos de oficina. ¡Descubre todo lo que
+              tenemos para ti!
+            </CardDescription>
+          </section>
+
+          <section>
+            <Image
+              src={lastProduct.productImage[1].url}
+              alt={`producto ${lastProduct.name}`}
+              width={500}
+              height={500}
+              className="object-cover"
+            ></Image>
+          </section>
+        </Card>
       </article>
 
       <article>
-        <h2 className={`text-xl font-bold ${fontSans.className} mb-2`}>
-          Productos
-        </h2>
-        <section className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-3">
+        <h3 className={`text-xl font-bold ${fontSans.className} mb-2`}>
+          Nuestros productos
+        </h3>
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
           {products.map((product) => (
-            <Card key={product.id}>
-              <CardHeader>
-                <Image
-                  src={product.productImage[0].url}
-                  alt={`producto ${product.name}`}
-                  width={300}
-                  height={300}
-                  className="w-80 h-40 object-cover"
-                />
-              </CardHeader>
+            <Link
+              key={product.id}
+              href={`/shop/product/${product.slug}`}
+              className={fontSans.className}
+            >
+              <Card>
+                <CardHeader>
+                  <Image
+                    src={product.productImage[0].url}
+                    alt={`producto ${product.name}`}
+                    width={500}
+                    height={500}
+                    className="object-cover"
+                  />
+                </CardHeader>
 
-              <CardContent className="space-y-1">
-                <Link
-                  href={`/shop/product/${product.slug}`}
-                  className={`hover:underline font-bold ${fontSans.className}`}
-                >
-                  {product.name}
-                </Link>
-                <CardDescription className="truncate">
-                  {product.description}
-                </CardDescription>
-                <p>{formatCurrency(product.price)}</p>
-              </CardContent>
+                <CardContent className="space-y-1">
+                  <h3 className="font-bold">{product.name}</h3>
 
-              <CardFooter>
-                <ProductsButtonCart product={product} />
-              </CardFooter>
-            </Card>
+                  <CardDescription className="truncate">
+                    {product.description}
+                  </CardDescription>
+                  <p>{formatCurrency(product.price)}</p>
+                </CardContent>
+              </Card>
+            </Link>
           ))}
         </section>
       </article>
