18cf7acb7b7a3bfc644d581e563ad4dc669ef278
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-28 18:22:44 -0500

    feat: Add lazy loading for images in PaymentPage component

--- app/(shop)/shop/payment/page.tsx
+++ app/(shop)/shop/payment/page.tsx
@@ -1,7 +1,21 @@
-export default function PaymentPage() {
+import { auth } from '@/auth.config'
+import {
+  BarProgress,
+  CardCheckoutItems, // CardItemsLoading,
+  CardCheckoutSummary // CardSummaryLoading
+} from '@/components'
+import { redirect } from 'next/navigation'
+
+export default async function PaymentPage() {
+  const user = await auth()
+  if (!user) redirect('/auth/login?redirect=/shop/checkout')
   return (
-    <div>
-      <h1>Si llegaste aquí se almaceno tus datos!</h1>
-    </div>
+    <section>
+      <BarProgress step={4} />
+      <article className="max-w-screen-2xl grid grid-cols-1 lg:grid-cols-3 gap-x-5 mx-auto p-5">
+        {/* <CardCheckoutItems />
+        <CardCheckoutSummary /> */}
+      </article>
+    </section>
   )
 }
