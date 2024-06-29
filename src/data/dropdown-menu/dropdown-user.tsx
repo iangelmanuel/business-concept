7639609ec050d4fb5cfe80ff7f7926feecd8fd7b
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-29 02:42:03 -0500

    ```text refactor: Add new links to dropdownUser in dropdown-user.tsx
    This commit adds new links to the dropdownUser array in the dropdown-user.tsx file. The new links include 'Mis Compras' and 'Mis direcciones' with corresponding hrefs and icons. This enhancement improves the user navigation options in the dropdown menu, providing easy access to purchases and addresses.

--- src/data/dropdown-menu/dropdown-user.tsx
+++ src/data/dropdown-menu/dropdown-user.tsx
@@ -1,10 +1,26 @@
-import { CreditCard, LockIcon, ShoppingBagIcon, User } from 'lucide-react'
+import {
+  BookUserIcon,
+  CreditCard,
+  LockIcon,
+  ShoppingBagIcon,
+  User
+} from 'lucide-react'
 
 export const dropdownUser = [
   {
     label: 'Cuenta',
     href: '/dashboard/profile',
     icon: <User className="mr-2 h-4 w-4" />
+  },
+  {
+    label: 'Mis Compras',
+    href: '/dashboard/purchases',
+    icon: <ShoppingBagIcon className="mr-2 h-4 w-4" />
+  },
+  {
+    label: 'Mis direcciones',
+    href: '/dashboard/addresses',
+    icon: <BookUserIcon className="mr-2 h-4 w-4" />
   },
   {
     label: 'Métodos de Pago',
@@ -15,10 +31,5 @@ export const dropdownUser = [
     label: 'Seguridad y Privacidad',
     href: '/dashboard/security-privacy',
     icon: <LockIcon className="mr-2 h-4 w-4" />
-  },
-  {
-    label: 'Mis Compras',
-    href: '/dashboard/purchases',
-    icon: <ShoppingBagIcon className="mr-2 h-4 w-4" />
   }
 ]
