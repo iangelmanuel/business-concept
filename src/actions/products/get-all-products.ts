51d744b90260ffd2a56595f8e52a1092801f816b
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-29 01:39:17 -0500

    feat: Filter products with stock greater than 0 in getAllProducts action
    This commit modifies the getAllProducts action in the products module to include a filter for products with a stock greater than 0. By adding the 'where' clause with the condition 'stock: { gt: 0 }', we ensure that only products with available stock are fetched from the database. This enhancement improves the functionality of the getAllProducts action by excluding products that are out of stock, providing more accurate and relevant results.

--- src/actions/products/get-all-products.ts
+++ src/actions/products/get-all-products.ts
@@ -4,6 +4,11 @@ import { prisma } from '@/lib'
 
 export async function getAllProducts() {
   const products = await prisma.product.findMany({
+    where: {
+      stock: {
+        gt: 0
+      }
+    },
     include: {
       productImage: true,
       category: true
