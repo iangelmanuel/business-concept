d3c85d0b54f3922ffec0627189df1b808535ea2a
iangelmanuel <iangelmanueldm@gmail.com>
2024-06-29 00:38:54 -0500

    feat: Update PurchasesPage to display user orders
    This commit updates the PurchasesPage component to display the user's orders. It adds the getOrdersByUser action to fetch the orders from the server and renders the order cards with relevant information such as order ID, total amount, date, and status. This enhancement improves the user experience by providing a clear overview of their purchases.

--- app/(user)/dashboard/purchases/page.tsx
+++ app/(user)/dashboard/purchases/page.tsx
@@ -1,7 +1,71 @@
-export default function PurchasesPage() {
+import { getOrdersByUser } from '@/actions'
+import {
+  Badge,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  buttonVariants
+} from '@/components'
+import { checkOrderStatus, formatCurrency, formatDate } from '@/utils'
+import Link from 'next/link'
+
+export default async function PurchasesPage() {
+  const orders = await getOrdersByUser()
   return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
+    <article>
+      {orders.length !== 0 ? (
+        <>
+          <h1 className="text-2xl font-bold mb-3">Mis compras</h1>
+
+          <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-5">
+            {orders.map((order) => (
+              <Card key={order.id}>
+                <CardHeader className="flex">
+                  <h2 className="font-bold">Orden: {order.id}</h2>
+                </CardHeader>
+
+                <CardContent>
+                  <h3 className="font-bold">Información</h3>
+                  <section className="text-sm text-muted-foreground">
+                    <div>
+                      <span className="font-bold">Importe:</span>{' '}
+                      <span>{formatCurrency(order.total)}</span>
+                    </div>
+
+                    <div>
+                      <span className="font-bold">Fecha:</span>{' '}
+                      <span>{formatDate(order.createdAt)}</span>
+                    </div>
+
+                    <div>
+                      <span className="font-bold">Estado:</span>{' '}
+                      <Badge variant={checkOrderStatus(order)}>
+                        {checkOrderStatus(order) === 'success'
+                          ? 'Aprobado'
+                          : checkOrderStatus(order) === 'pending'
+                            ? 'Pendiente'
+                            : 'Rechazado'}
+                      </Badge>
+                    </div>
+                  </section>
+                </CardContent>
+
+                <CardFooter className="flex justify-end items-center">
+                  <Link
+                    href={`/dashboard/purchases/order/${order.id}`}
+                    className={buttonVariants()}
+                  >
+                    Ver detalles
+                  </Link>
+                </CardFooter>
+              </Card>
+            ))}
+          </section>
+        </>
+      ) : (
+        <h1 className="text-2xl font-bold">No se ha registrado compras</h1>
+      )}
+    </article>
   )
 }
