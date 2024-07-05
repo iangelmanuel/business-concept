06b89569079ff5c5c322f52f4351aec22dd2d37e
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-04 19:20:48 -0500

    refactor: Rename admin-aside.tsx to auth-aside.tsx and remove user-aside.tsx

--- src/components/general/aside/auth-aside.tsx
+++ src/components/general/aside/auth-aside.tsx
@@ -1,39 +1,38 @@
 'use client'
 
-import { titleFont } from '@/config'
 import { dropdownAdmin, dropdownUser } from '@/data'
 import { cn } from '@/lib'
+import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export const AdminAside = () => {
+export const AuthAside = () => {
+  const { data: session } = useSession()
   const pathname = usePathname()
+
+  const isAdmin = session?.user.role.includes('admin')
+
   return (
     <aside className="sticky top-0 col-span-2 hidden h-screen border-r xl:block">
       <section className="mt-5 space-y-2 p-2">
-        <p className={`${titleFont.className} font-bold`}>
-          Funcionalidades administrativas
-        </p>
+        {isAdmin &&
+          dropdownAdmin.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className={cn(
+                'flex items-center p-3',
+                pathname === item.href
+                  ? 'bg-gray-100 dark:bg-gray-900'
+                  : 'hover:bg-gray-100 hover:dark:bg-gray-900'
+              )}
+            >
+              {item.icon}
+              <span>{item.label}</span>
+            </Link>
+          ))}
 
-        {dropdownAdmin.map((item, index) => (
-          <Link
-            key={index}
-            href={item.href}
-            className={cn(
-              'flex items-center p-3',
-              pathname === item.href
-                ? 'bg-gray-100 dark:bg-gray-900'
-                : 'hover:bg-gray-100 hover:dark:bg-gray-900'
-            )}
-          >
-            {item.icon}
-            <span>{item.label}</span>
-          </Link>
-        ))}
-
-        <p className={`${titleFont.className} mt-5 font-bold`}>
-          Funcionalidades de usuario
-        </p>
+        {isAdmin && <div className="border-b" />}
 
         {dropdownUser.map((item, index) => (
           <Link
