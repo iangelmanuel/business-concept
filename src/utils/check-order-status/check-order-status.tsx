2ec9493146b460562cf5e1372a8800bd40905266
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-07 22:42:31 -0500

    refactor: Update checkOrderStatusCn function to handle different order statuses
    Update the `checkOrderStatusCn` function in the `check-order-status.tsx` file to handle different order statuses. This change improves the functionality of the function by using a switch statement to determine the appropriate status based on the input. It ensures that the function returns the correct status for each order status, including 'pending', 'destructive', and 'success'.

--- src/utils/check-order-status/check-order-status.tsx
+++ src/utils/check-order-status/check-order-status.tsx
@@ -1,16 +1,29 @@
 import type { Order } from '@prisma/client'
 import type { UserOrder } from '@/types'
 
-export function checkOrderStatus(orderItem: UserOrder | Order) {
-  if (orderItem.isPaid) {
-    return 'success'
-  }
+export function checkOrderStatusCn(
+  orderStatus: UserOrder['orderStatus'] | Order
+) {
+  switch (orderStatus) {
+    case 'pending':
+      return 'pending'
 
-  if (!orderItem.isPaid && !orderItem.transactionId) {
-    return 'pending'
-  }
+    case 'processing':
+      return 'pending'
 
-  if (!orderItem.isPaid && orderItem.transactionId) {
-    return 'destructive'
+    case 'cancelled':
+      return 'destructive'
+
+    case 'approved':
+      return 'success'
+
+    case 'shipped':
+      return 'destructive'
+
+    case 'delivered':
+      return 'success'
+
+    default:
+      return 'pending'
   }
 }
