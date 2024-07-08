5612c9c577037f7ca1d18c890e6ce6073e859b21
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-08 00:41:36 -0500

    refactor: Update badge component styles for new order status labels
    Update the styles of the badge component to accommodate the new order status labels: 'processing', 'shipped', and 'delivered'. This change ensures that the badge component displays the correct background color and text color for each order status, improving the visual representation of the status labels.

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -11,16 +11,22 @@ const badgeVariants = cva(
           'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
         secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        success:
-          'border-transparent bg-emerald-500 text-destructive-foreground hover:bg-emerald-500/80',
+        outline: 'text-foreground',
         pending:
           'border-transparent bg-yellow-500 text-destructive-foreground hover:bg-yellow-500/80',
+        processing:
+          'border-transparent bg-gray-500 text-destructive-foreground hover:bg-gray-500/80',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        outline: 'text-foreground',
-        admin:
+        success:
+          'border-transparent bg-emerald-500 text-destructive-foreground hover:bg-emerald-500/80',
+        shipped:
+          'border-transparent bg-sky-500 text-destructive-foreground hover:bg-sky-500/80',
+        delivered:
           'border-transparent bg-orange-500 text-destructive-foreground hover:bg-orange-500/80',
-        user: 'border-transparent bg-sky-600 text-destructive-foreground hover:bg-sky-600/80'
+        admin:
+          'border-transparent bg-purple-500 text-destructive-foreground hover:bg-purple-500/80',
+        user: 'border-transparent bg-indigo-600 text-destructive-foreground hover:bg-indigo-600/80'
       }
     },
     defaultVariants: {
