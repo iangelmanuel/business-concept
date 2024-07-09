6c80ffd6fbe5eb07bc9faffc5c54167be0631da7
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-08 22:33:35 -0500

    refactor: Update section height in AdminLayout for better user experience
    Update the section height in the AdminLayout component to "h-screen" for a better user experience. This change ensures that the section takes up the full height of the screen, providing a more immersive and visually appealing layout for the admin pages.

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -17,14 +17,14 @@ export default async function AdminLayout({
   children: React.ReactNode
 }>) {
   const session = await auth()
-  const userRole = session?.user.role === 'user'
+  const userRole = session?.user.role.includes('user')
   if (userRole || !session) return redirect('/auth/login')
 
   return (
     <>
       <Header />
       <main>
-        <section className="gap-10 p-5 xl:grid xl:grid-cols-12 2xl:p-0">
+        <section className="h-screen gap-10 p-5 xl:grid xl:grid-cols-12 2xl:p-0">
           <AdminAside />
           <Card className="mt-5 overflow-y-auto p-3 xl:col-span-9 xl:h-[650px] xl:p-10 2xl:h-[850px]">
             {children}
