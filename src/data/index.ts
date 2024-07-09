1b4572afa5d4c0d9cee5666dfd644931540b3884
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-09 18:02:38 -0500

    refactor: Add select option data for order status and order tracking company
    Add select option data for order status and order tracking company in the data directory. This change includes the creation of two new files, select-order-status.ts and select-order-tracking-company.ts, which export arrays of objects representing the available options for order status and order tracking company respectively. This addition ensures that the application has the necessary data to populate select dropdowns for these fields.

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,3 +4,6 @@ export * from './navigation/navigation-items'
 export * from './dropdown-menu/dropdown-user'
 export * from './dropdown-menu/dropdown-admin'
 export * from './dropdown-menu/dropdown-general'
+
+export * from './select-option/select-order-status'
+export * from './select-option/select-order-tracking-company'
---
+++ src/data/select-option/select-order-tracking-company.ts
@@ -0,0 +1,7 @@
+export const selectOrderTrackingCompanyData = [
+  { value: 'envia', label: 'Envia' },
+  { value: 'servientrega', label: 'ServiEntrega' },
+  { value: 'inter rapidísimo', label: 'Inter Rapidísimo' },
+  { value: 'coordinadora', label: 'Coordinadora' },
+  { value: 'dhl', label: 'DHL' }
+]
