99c52e45eb530f64872219b1afbeaacf56fd9b81
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-13 06:36:54 -0500

    feat: Add discount converter utility function
    Add a new utility function, `utilDiscountConverter`, to the `discount-converter.ts` file in the `src/utils/discount-converter` directory. This function calculates the discounted price and formats the currency for a given product's price and discount. It also determines if the product has a discount and calculates the percentage of the discount. This utility function enhances the functionality of the application by providing a convenient way to handle product discounts.

---
+++ src/utils/discount-converter/discount-converter.ts
@@ -0,0 +1,41 @@
+import { formatCurrency } from '../format-currency/format-currency'
+import type { ProductType } from '@/types'
+
+function discountConverter(discount: ProductType['discount']) {
+  if (discount === 1) {
+    return '0%'
+  } else {
+    return `${discount * 100}%`
+  }
+}
+
+function getProductDiscount(
+  price: ProductType['price'],
+  discount: ProductType['discount']
+) {
+  if (discount === 1) {
+    return price
+  } else {
+    return price * discount
+  }
+}
+
+export function utilDiscountConverter(
+  discount: ProductType['price'],
+  price: ProductType['discount']
+) {
+  const isProductWithDiscount = discount === 1 // TRUE OR FALSE
+  const priceWithDiscount = getProductDiscount(price, discount) // NUMBER
+
+  const priceBeforeFormatted = formatCurrency(price)
+  const priceAfterFormatted = formatCurrency(price - priceWithDiscount)
+  const percentageOfDiscount = discountConverter(discount)
+
+  return {
+    isProductWithDiscount,
+    priceWithDiscount,
+    priceBeforeFormatted,
+    priceAfterFormatted,
+    percentageOfDiscount
+  }
+}
