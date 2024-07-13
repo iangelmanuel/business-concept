e2ade42848404a45e1716cb2f98848b8f145c4b0
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-13 06:38:09 -0500

    feat: Add "not-found" page for admin/products
    Add a new "not-found" page component for the admin/products directory. This page is displayed when a product is not found in the database. It includes metadata for SEO purposes and provides a user-friendly message with a link to return to the homepage. This change enhances the functionality and user experience of the application.

---
+++ app/(admin)/admin/products/not-found.tsx
@@ -0,0 +1,41 @@
+// import Image from 'next/image'
+import type { Metadata } from 'next'
+import Link from 'next/link'
+import { titleFont } from '@/config'
+
+export const metadata: Metadata = {
+  title: 'Producto no encontrado - Business Concept',
+  description:
+    'Lo sentimos mucho, pero el producto que buscas no se encuentra en nuestra base de producto. ¡Regresa al inicio!',
+  keywords: 'dato, no encontrado, no existe, no se encuentra'
+}
+
+export default function PageNotFound() {
+  return (
+    <section className="flex h-[800px] w-full flex-col-reverse items-center justify-center align-middle md:flex-row">
+      <div className="mx-5 px-5 text-center">
+        <h2 className={`${titleFont.className} text-9xl antialiased`}>404</h2>
+        <p className="text-xl font-semibold">Whoops! Lo sentimos mucho.</p>
+        <p className="font-light">
+          <span>Puedes regresar al </span>
+          <Link
+            href="/admin/products"
+            className="font-normal transition-all hover:underline"
+          >
+            Inicio
+          </Link>
+        </p>
+      </div>
+
+      {/* <div className="px-5 mx-5">
+        <Image
+          src="/imgs/starman_750x750.png"
+          alt="Starman"
+          width={550}
+          height={550}
+          className="p-5 sm:p-0"
+        />
+      </div> */}
+    </section>
+  )
+}
