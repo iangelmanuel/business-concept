52c4ef486cbca26478d8eb8da44b4790c037cf51
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-15 05:13:20 -0500

    feat: Update ProductImage schema and types
    Update the ProductImage schema and types to include the new "publicId" field. This field allows for storing the public ID of the image in Cloudinary for easier retrieval and management. This enhancement improves the functionality of the application by providing a way to associate Cloudinary public IDs with product images.

--- src/types/products/products.d.ts
+++ src/types/products/products.d.ts
@@ -1,5 +1,10 @@
-import type { ProductGeneralSchema, ProductsSchema } from '@/schema'
+import type {
+  ProductGeneralSchema,
+  ProductImage,
+  ProductsSchema
+} from '@/schema'
 import type { z } from 'zod'
 
 export type ProductType = z.infer<typeof ProductGeneralSchema>
 export type ProductAllType = z.infer<typeof ProductsSchema>
+export type ProductImage = z.infer<typeof ProductImage>
