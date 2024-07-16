8615e88f6f3ce2535bf454c222ade8d0de140b62
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-16 03:57:39 -0500

    feat: Add discount column to Order and OrderItem models
    This commit adds a new "discount" column to the "Order" and "OrderItem" models in the database schema. The "discount" column is of type DOUBLE PRECISION and has a default value of 0 for "Order" and 1 for "OrderItem". This enhancement allows for applying discounts to orders and order items, improving the functionality of the application.

---
+++ prisma/migrations/20240716000730_add_discount_to_order_order_item_models/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "discount" DOUBLE PRECISION NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "OrderItem" ADD COLUMN     "discount" DOUBLE PRECISION NOT NULL DEFAULT 1;
