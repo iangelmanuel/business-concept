f59aa140dee56c92a9132d1db2bcf668217835da
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-17 05:36:48 -0500

    refactor: Add createSlugForProduct utility function
    The code changes add a new utility function called createSlugForProduct. This function takes a product name as input and generates a slug by removing diacritical marks, replacing spaces with hyphens, and converting the name to lowercase. This utility function will be useful for generating slugs for product URLs.

---
+++ src/utils/create-slug-for-product/creaet-slug-for-product.ts
@@ -0,0 +1,9 @@
+export function createSlugForProduct(name: string) {
+  const slug = name
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/\s+/g, '-')
+    .toLowerCase()
+
+  return slug
+}
