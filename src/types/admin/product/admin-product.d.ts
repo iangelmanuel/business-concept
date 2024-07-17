76d77333f212be94ffc27e49b8b7f47de7c21f3b
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-17 05:37:19 -0500

    refactor: Update ProductCreateForm type to include images property
    The code changes in admin-product.d.ts modify the ProductCreateForm type to include an images property of type FormData or File[]. This change allows for the uploading of images when creating a new product. It enhances the functionality of the product management feature in the application.

--- src/types/admin/product/admin-product.d.ts
+++ src/types/admin/product/admin-product.d.ts
@@ -1,5 +1,8 @@
 import type { ProductCreateSchema, ProductUpdateSchema } from '@/schema'
 import type { z } from 'zod'
 
-export type ProductCreateForm = z.infer<typeof ProductCreateSchema>
+export type ProductCreateForm = z.infer<typeof ProductCreateSchema> & {
+  images: FormData | File[]
+}
+
 export type ProductUpdateForm = z.infer<typeof ProductUpdateSchema>
