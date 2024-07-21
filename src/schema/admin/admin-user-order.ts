c4da4f85e931ead1514525b5201a8c306b45b0f7
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-20 20:18:48 -0500

    refactor: Add AdminDashboardSchema to admin-user-order.ts
    This commit adds the `AdminDashboardSchema` to the `admin-user-order.ts` file. The schema includes properties for `id`, `total`, `discount`, `orderStatus`, `paidAt`, `createdAt`, and `user`. This refactor enhances the codebase by providing a structured schema for the admin dashboard and improves code organization.

--- src/schema/admin/admin-user-order.ts
+++ src/schema/admin/admin-user-order.ts
@@ -1,4 +1,4 @@
-import { UserOrderSchema } from '../order-schema'
+import { OrderGeneralSchema, UserOrderSchema } from '../order-schema'
 import { UserGeneralSchema } from '../user-schema'
 import { z } from 'zod'
 
@@ -12,4 +12,18 @@ export const UserOrderByAdmin = z.object({
     })
     .nullable(),
   user: UserGeneralSchema
+})
+
+export const AdminDashboardSchema = z.object({
+  id: OrderGeneralSchema.shape.id,
+  total: OrderGeneralSchema.shape.total,
+  discount: OrderGeneralSchema.shape.discount,
+  orderStatus: OrderGeneralSchema.shape.orderStatus,
+  paidAt: OrderGeneralSchema.shape.paidAt,
+  createdAt: OrderGeneralSchema.shape.createdAt,
+  user: z.object({
+    name: UserGeneralSchema.shape.name,
+    lastname: UserGeneralSchema.shape.lastname,
+    email: UserGeneralSchema.shape.email
+  })
 })
