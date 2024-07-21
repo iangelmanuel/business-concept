7fb6f435f377b4b14ec64af6a6a1956d2f7b4622
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-20 20:19:39 -0500

    refactor: Update import paths and components for admin profile dashboard
    This commit updates the import paths and components in the `admin/profile/page.tsx` file to improve code organization and enhance the admin profile dashboard. The `getSpecificsOrderDatas` and `getSpecificsUserDatas` functions are imported from the `@/actions` module. The `GeneralFinanceCards`, `OrderWithApproveStatus`, and `PrincipalContent` components are imported from the `@/components` module. These changes ensure that the necessary data is retrieved and displayed correctly in the admin profile dashboard.

--- app/(admin)/admin/profile/page.tsx
+++ app/(admin)/admin/profile/page.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
-import { auth } from '@/auth.config'
-import { Card, CardContent, CardHeader } from '@/components'
-import { titleFont } from '@/config'
-import { formatDate } from '@/utils'
+import { getSpecificsOrderDatas, getSpecificsUserDatas } from '@/actions'
+import {
+  GeneralFinanceCards,
+  OrderWithApproveStatus,
+  PrincipalContent
+} from '@/components'
 
 export const metadata: Metadata = {
   title: 'Admin Perfil - Business Concept',
@@ -10,61 +12,17 @@ export const metadata: Metadata = {
 }
 
 export default async function ProfilePage() {
-  const session = await auth()
-  const user = session!.user
+  const data = await getSpecificsOrderDatas()
+  const userData = await getSpecificsUserDatas()
+
   return (
-    <section>
-      <Card className="mb-5">
-        <CardHeader>
-          <h1
-            className={`${titleFont.className} text-center text-2xl font-bold sm:text-6xl`}
-          >
-            Bienvenido al perfil Administrativo de Business Concept
-          </h1>
-        </CardHeader>
-      </Card>
-
-      <Card className="mx-auto max-w-screen-md">
-        <CardHeader>
-          <h2
-            className={`${titleFont.className} text-center text-xl font-semibold`}
-          >
-            Perfil de {user.name} {user.lastname}
-          </h2>
-        </CardHeader>
-
-        <CardContent>
-          <div className="flex items-center justify-between">
-            <p className="font-bold">ID:</p>
-            <p>{user.id}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="font-bold">Email:</p>
-            <p>{user.email}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="font-bold">Teléfono:</p>
-            <p>{user.phone}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="font-bold">Rol:</p>
-            <p>{user.role}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="font-bold">Fecha de Creación:</p>
-            <p>{formatDate(user.createdAt)}</p>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="font-bold">Fecha de Actualización:</p>
-            <p>{formatDate(user.updatedAt)}</p>
-          </div>
-        </CardContent>
-      </Card>
+    <section className="grid grid-cols-1 gap-5 lg:grid-cols-2">
+      <GeneralFinanceCards
+        data={data}
+        userId={userData}
+      />
+      <PrincipalContent data={data} />
+      <OrderWithApproveStatus data={data} />
     </section>
   )
 }
