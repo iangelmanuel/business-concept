2433adecc706b6dd6c5e150247db81c208d344a0
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-22 05:49:29 -0500

    refactor: Update contact.d.ts with ContactFormType
    This commit updates the contact.d.ts file to include the ContactFormType, which is inferred from the CreateContactSchema. This refactor enhances the codebase by providing a structured type for contact form data and improves code organization.

--- src/types/contact.d.ts
+++ src/types/contact.d.ts
@@ -1,5 +1,10 @@
-import type { ContactFullDataSchema, ContactGeneralSchema } from '@/schema'
+import type {
+  ContactFullDataSchema,
+  ContactGeneralSchema,
+  CreateContactSchema
+} from '@/schema'
 import type { z } from 'zod'
 
 export type ContactType = z.infer<typeof ContactGeneralSchema>
 export type ContactAllType = z.infer<typeof ContactFullDataSchema>
+export type ContactFormType = z.infer<typeof CreateContactSchema>
